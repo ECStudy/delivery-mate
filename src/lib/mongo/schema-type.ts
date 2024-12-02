e69d57813ee2be2b38b21c03321c6a4284af3a8c
23092jongsik <jongsik@ecount.co.kr>
2024-12-02 18:14:33 +0900

    add some schemas

--- src/lib/mongo/schema-type.ts
+++ src/lib/mongo/schema-type.ts
@@ -1,24 +1,57 @@
 export type OurDate = Date | string;
 
-// mongo test collection schema type
-export interface ITest {
-  name: string;
-  age: number;
-}
-
-type IMember = {
-  id: string;
-  name: string;
+type IPartyMember = {
+  user: IUser;
+  price: number;
+  status: string;
 };
 
 export interface IParty {
-  member: IMember[];
+  member: IPartyMember[];
   title: string;
   description: string;
   thumbnail: string;
   tags: string[];
   maxMember: number;
   limitTime: number;
+  status: string;
+  due: [OurDate, OurDate];
+  createdAt: OurDate;
+  updatedAt: OurDate;
+  deletedAt: OurDate | null;
+}
+
+export interface IHistory {
+  party: IParty;
+  createdAt: OurDate;
+  updatedAt: OurDate;
+  deletedAt: OurDate | null;
+}
+
+export interface IUser {
+  email: string;
+  name: string;
+  nickname: string;
+  accessToken: string;
+  history: IHistory[];
+  createdAt: OurDate;
+  updatedAt: OurDate;
+  deletedAt: OurDate | null;
+}
+
+export interface IRestaurants {
+  name: string;
+  address: string;
+  phone: string;
+  category: string;
+  thumbnail: string;
+  createdAt: OurDate;
+  updatedAt: OurDate;
+  deletedAt: OurDate | null;
+}
+
+export interface ICategories {
+  name: string;
   createdAt: OurDate;
   updatedAt: OurDate;
   deletedAt: OurDate | null;
