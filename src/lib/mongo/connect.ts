372e9dc925ecc353e822ff851e2ac3c5ed625338
Minji Kim <wisdomin121@naver.com>
2024-12-18 23:48:23 +0900

    [ fix ] build error 3

--- src/lib/mongo/connect.ts
+++ src/lib/mongo/connect.ts
@@ -3,6 +3,10 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI || 'mongodb://root:example@localhost:27017';
 const options = {};
 
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env local');
+}
+
 let client;
 let clientPromise: Promise<MongoClient>;
 
@@ -13,10 +17,6 @@ if (process.env.NODE_ENV === 'development') {
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your Mongo URI to .env.local');
-  }
-
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
